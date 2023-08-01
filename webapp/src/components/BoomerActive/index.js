a8e1ec899c69e36997b0556fae6f5f302d3d0645
Angelo CG <angelocas13_8@hotmail.com>
2023-08-01 15:17:38 -0600

    feat(webapp): improved BoomerActive component

--- webapp/src/components/BoomerActive/index.js
+++ webapp/src/components/BoomerActive/index.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
-import { Button, Typography } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+import InputAdornment from '@mui/material/InputAdornment'
+import { Button, Typography, OutlinedInput } from '@mui/material'
+import ChangeCircleOutlinedIcon from '@mui/icons-material/ChangeCircleOutlined'
 
 import { useSharedState } from '../../context/state.context'
 
-const BoomerActive = () => {
-  const [state] = useSharedState()
+import styles from './styles'
 
-  const data = [
-    { token: '200', symbol: 'eVMPX' },
-    { token: '198', symbol: 'bVMPX' }
+const useStyles = makeStyles(styles)
+
+const BoomerActive = () => {
+  const classes = useStyles()
+  const [state] = useSharedState()
+  const [firstToken, setFirstToken] = useState({ amount: 0, symbol: 'eVMPX' })
+  const [secondToken, setSecondToken] = useState({ amount: 0, symbol: 'bVMPX' })
+
+  const balances = [
+    { token: '100', symbol: 'eVMPX' },
+    { token: '100', symbol: 'bVMPX' }
   ]
+
+  const handleFlip = () => {
+    const tokenOne = firstToken
+    const tokenTwo = secondToken
+
+    setFirstToken(tokenTwo)
+    setSecondToken(tokenOne)
+  }
+
+  const handleSetevmpx = value => {
+    if (firstToken.symbol === 'eVMPX') {
+      setFirstToken({ ...firstToken, amount: value })
+      setSecondToken({ ...secondToken, amount: value * 2 })
+    } else {
+      setFirstToken({ ...firstToken, amount: value })
+      setSecondToken({ ...secondToken, amount: value / 2 })
+    }
+  }
+
+  const handleSetbvmpx = value => {
+    if (secondToken.symbol === 'bVMPX') {
+      setSecondToken({ ...secondToken, amount: value })
+      setFirstToken({ ...firstToken, amount: value / 2 })
+    } else {
+      setSecondToken({ ...secondToken, amount: value })
+      setFirstToken({ ...firstToken, amount: value * 2 })
+    }
+  }
 
   return (
     <Box
@@ -21,7 +59,18 @@ const BoomerActive = () => {
       width="100%"
       padding={3}
     >
-      <Box display="flex" justifyContent="flex-end">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          <Typography variant="body1">Balances:</Typography>
+          {balances.map(item => (
+            <Box display="flex" justifyContent="center" key={item.symbol}>
+              <Typography marginRight={2} variant="body2">
+                {item.token}
+              </Typography>
+              <Typography variant="body2">{item.symbol}</Typography>
+            </Box>
+          ))}
+        </Box>
         <Box>
           <Typography variant="body1">{state.user}</Typography>
           <Typography variant="body1">0x5622...hg6</Typography>
@@ -31,18 +80,63 @@ const BoomerActive = () => {
         justifyContent="center"
         flexDirection="column"
         display="flex"
+        paddingX={20}
         mt={2}
-        paddingX={24}
       >
-        {data.map(item => (
-          <Box display="flex" justifyContent="center" key={item.symbol}>
-            <Typography marginRight={2} variant="body1">
-              {item.token}
-            </Typography>
-            <Typography variant="body1">{item.symbol}</Typography>
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Box display="flex" marginY={1} justifyContent="center">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              value={firstToken.amount}
+              type="number"
+              onChange={e => {
+                let value = e.target.value
+
+                if (value < 0) {
+                  value = 0
+                }
+
+                handleSetevmpx(value)
+              }}
+              endAdornment={
+                <InputAdornment position="end">
+                  {firstToken.symbol}
+                </InputAdornment>
+              }
+            />
           </Box>
-        ))}
-        <br />
+          <ChangeCircleOutlinedIcon
+            fontSize="large"
+            onClick={() => handleFlip()}
+            className={classes.flipStyle}
+          />
+          <Box display="flex" marginY={1} justifyContent="center">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              value={secondToken.amount}
+              type="number"
+              onChange={e => {
+                let value = e.target.value
+
+                if (value < 0) {
+                  value = 0
+                }
+
+                handleSetbvmpx(value)
+              }}
+              endAdornment={
+                <InputAdornment position="end">
+                  {secondToken.symbol}
+                </InputAdornment>
+              }
+            />
+          </Box>
+        </Box>
         <Button variant="outlined">Swap</Button>
       </Box>
       <Box mt={8} display="flex" justifyContent="space-between">

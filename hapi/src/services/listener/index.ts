ebafcdb659f9bfe3ced5ee0ba4b5c649df37fe92
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2023-08-29 14:45:43 -0600

    perf(hapi): handle chain queues

--- hapi/src/services/listener/index.ts
+++ hapi/src/services/listener/index.ts
@@ -3,14 +3,18 @@ import workerService from '../worker'
 import ethListenerService from './eth-listener'
 import payerService from '../payer'
 import catcherService from '../catcher'
+import { timeUtil } from '../../utils'
 
 const start = async () => {
   workerService.run(hyperionService.syncWorker())
   workerService.run(payerService.workerFetcher())
 
-  // sync with catcher and later with eth-listener
-  await workerService.run(catcherService.catchWorker())
+  // sync with eth-listener and later with catcher
+  // eth-listener will call the logic to check how many blocks are missing
+  // since the last time the server was running
   ethListenerService.listenForEvents()
+  await timeUtil.sleep(5)
+  workerService.run(catcherService.catchWorker())
 }
 
 export default { start }
--- hapi/src/services/queue-sync.ts
+++ hapi/src/services/queue-sync.ts
@@ -1,33 +1,53 @@
 import { queueSyncModel, queueModel } from '../models'
+import { ethConfig } from '../config'
 
-const addNewQueueSync = async (currentBlock: number) => {
+export const addNewQueueSync = async (currentBlock: number) => {
   const lastBlock = await queueModel.queries.getLastBlockNumber(
     queueModel.interfaces.Operation.pegin
   )
 
-  if (lastBlock === 0 || lastBlock === currentBlock) {
+  if (
+    lastBlock === currentBlock ||
+    ethConfig.startingBlockNumber >= currentBlock
+  ) {
     console.log('Skipping sync queue, no blocks to sync...')
 
     return
   }
 
-  const fromBlock = lastBlock + 1
+  const maxStoredToBlock = await queueSyncModel.queries.getMaxToBlock()
+  let fromBlock =
+    lastBlock !== 0 ? lastBlock + 1 : ethConfig.startingBlockNumber
   const toBlock = currentBlock - 1
+
+  if (maxStoredToBlock >= fromBlock) {
+    fromBlock = maxStoredToBlock + 1
+  }
+
+  if (fromBlock == toBlock) {
+    console.log('Skipping sync queue, no blocks to sync...')
+
+    return
+  }
 
   try {
     await queueSyncModel.queries.save({
-      to_block: toBlock,
       from_block: fromBlock,
+      to_block: toBlock,
       total_synced: 0
     })
 
-    console.log(`🎫🎫🎫 New queue sync added: ${fromBlock} - ${toBlock}`)
+    console.log(
+      `🎫🎫🎫 New queue sync added: ${fromBlock} - ${toBlock}: ${
+        toBlock - fromBlock
+      }`
+    )
   } catch (error) {
     console.log(`🔥🔥🔥 Failed to save queue sync: ${error}`)
   }
 }
 
-const markCompleted = async (id: string, totalSynced: number) => {
+export const markCompleted = async (id: string, totalSynced: number) => {
   try {
     await queueSyncModel.queries.update(
       id,
